7471e61f0329c7447b65955987d55b868454447f
Yaqoob Jamal <yaqoob.jamal@yahoo.com>
2022-05-31 16:46:19 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# ClickSend Mule 4 s Extension
+
+Add description ...
+
+
+...
+
+
+...
+
+
+Add this dependency to your application pom.xml
+
+```
+<groupId>org.clicksend.extensions</groupId>
+<artifactId>mule_clicksend_connectors</artifactId>
+<version>1.0</version>
+<classifier>mule-plugin</classifier>
+```
---
+++ src/main/java/org/clicksend/internal/ClickSendMule4sConfiguration.java
@@ -0,0 +1,38 @@
+package org.clicksend.internal;
+
+import org.mule.runtime.extension.api.annotation.Operations;
+import org.mule.runtime.extension.api.annotation.connectivity.ConnectionProviders;
+import org.mule.runtime.extension.api.annotation.param.Parameter;
+import org.mule.runtime.extension.api.annotation.param.display.DisplayName;
+import org.mule.runtime.extension.api.annotation.param.display.Example;
+import org.mule.runtime.extension.api.annotation.param.display.Password;
+import org.springframework.core.annotation.Order;
+
+/**
+ * This class represents an extension configuration, values set in this class are commonly used across multiple
+ * operations since they represent something core from the extension.
+ */
+@Operations(ClickSendMule4sOperations.class)
+@ConnectionProviders(ClickSendMule4sConnectionProvider.class)
+public class ClickSendMule4sConfiguration {
+
+  @Parameter
+  @DisplayName(value = "Username/Email")
+  @Order(value = 1)
+  @Example(value = "abc@xyz.com")
+  private String userId;
+  
+  @Parameter
+  @DisplayName(value = "Password")
+  @Order(value = 2)
+  @Password
+  private String password;
+
+  public String getUserId(){
+    return userId;
+  }
+  
+  public String getPassword(){
+	    return password;
+	  }
+}
